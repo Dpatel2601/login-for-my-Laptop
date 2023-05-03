3a46c0e2c00cccdec81685f6f2cf1704814402c7
Dpatel2601 <2601dpatel@gmail.com>
2023-05-03 19:58:14 +0100

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile } from "firebase/auth";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRQXHdYCmUUepkq_G82t7YekT1XxbVHDQ",
+  authDomain: "loginform-c45c4.firebaseapp.com",
+  projectId: "loginform-c45c4",
+  storageBucket: "loginform-c45c4.appspot.com",
+  messagingSenderId: "14636549774",
+  appId: "1:14636549774:web:a5aa4c13119c0a30eaabdf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const storage = getStorage();
+
+export function signup(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export function login(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function logout() {
+  return signOut(auth);
+}
+
+// Custom Hook
+export function useAuth() {
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, user => setCurrentUser(user));
+    return unsub;
+  }, [])
+
+  return currentUser;
+}
+
+// Storage
+export async function upload(file, currentUser, setLoading) {
+  const fileRef = ref(storage, currentUser.uid + '.png');
+
+  setLoading(true);
+  
+  const snapshot = await uploadBytes(fileRef, file);
+  const photoURL = await getDownloadURL(fileRef);
+
+  updateProfile(currentUser, {photoURL});
+  
+  setLoading(false);
+  alert("Uploaded file!");
+}
+
+
+

5783dae92acdcc5b65864bf496fe37b4f55b3f84
Dpatel2601 <2601dpatel@gmail.com>
2023-05-04 12:48:58 +0100

    second commit

---
+++ src/Nav-bar/SidebarData.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import * as AiIcons from "react-icons/ai";
+
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+]
--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet,  } from "react-router-dom";
+import Home from "./router/Home";
+import Navbar from "./Nav-bar/Navbar";
+import Signup from "./router/Signup";
+import Login from "./router/Login";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const AppLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
+
+
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "Signup",
+        element: <Signup />,
+      },
+      {
+        path: "Login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );

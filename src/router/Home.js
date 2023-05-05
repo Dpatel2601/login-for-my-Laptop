68198fc8eb6f8cb534c36c4bd1a7f3a080cf5930
Dpatel2601 <2601dpatel@gmail.com>
2023-05-05 12:55:34 +0100

    Six commit

--- src/router/Home.js
+++ src/router/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const Login = useNavigate();
+  const Signup = useNavigate();
   return (
     <div>
-      Home
+      <button id='LoginBTN' onClick={() => Login("/Login")}>Login</button>
+      <button id='SignupBTN' onClick={() => Signup("/Signup")}> Signup </button>
     </div>
   )
 }
